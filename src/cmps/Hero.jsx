a07c5654880c1909606a81728f5f12fba9b1d156
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-01-30 13:40:40 +0200

    Create hero section for the app

--- src/cmps/Hero.jsx
+++ src/cmps/Hero.jsx
@@ -1,11 +1,15 @@
 import { TypeWriter } from './TypeWrtiter';
-
+import mainImage from '../assets/imgs/main2.jpg';
 export function Hero() {
+  const linearGradientStyle = {
+    background: `linear-gradient(rgb(44, 174, 210, 0.7), rgba(0, 0, 0, 0.7)), url(${mainImage}) no-repeat center center/cover`,
+  };
   return (
-    <div className="hero-image">
+    <div className="hero-image main-layout full" style={linearGradientStyle}>
       <div className="hero-text">
         <TypeWriter />
-        <p>Your go-to app for effortless travel planning and discovering the perfect trek</p>
+        <p>Your go-to app for effortless travel planning and discovering the perfect trek.</p>
+        <a href="#tours"><button className='explore-btn'>explore tours</button></a>
       </div>
     </div>
   )
