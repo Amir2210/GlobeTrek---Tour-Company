745679cf47ebb4a72be05ec67ad1abecc9a70cbd
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-01-30 18:32:47 +0200

    Crate tours data

---
+++ src/data/data.js
@@ -0,0 +1,48 @@
+import tour1 from '../assets/imgs/tour-1.jpeg';
+import tour2 from '../assets/imgs/tour-2.jpeg';
+import tour3 from '../assets/imgs/tour-3.jpeg';
+import tour4 from '../assets/imgs/tour-4.jpeg';
+
+
+export const tours = [
+  {
+    id: 1,
+    image: tour1,
+    date: 'august 26th, 2020',
+    title: 'Tibet Adventure',
+    info: `Embark on a captivating journey through the stunning landscapes of Tibet with our Adventure Trek. Traverse majestic mountain trails, discover ancient monasteries, and immerse yourself in the unique culture of the region. From the spiritual ambiance of Lhasa to challenging high-altitude passes, this trek offers an unforgettable blend of natural beauty and cultural exploration, promising an extraordinary adventure in the heart of Tibet.`,
+    location: 'china',
+    duration: 6,
+    cost: 2100,
+  },
+  {
+    id: 2,
+    image: tour2,
+    date: 'october 1th, 2020',
+    title: 'best of Hawaii',
+    info: `Experience the tropical paradise of Hawaii with our captivating Hawaii Adventure. Immerse yourself in the lush landscapes, pristine beaches, and vibrant culture of these enchanting islands. From exploring volcanic wonders to enjoying the warmth of the Pacific, our Hawaii Adventure promises an unforgettable journey filled with natural beauty and island charm. Discover the aloha spirit as you embark on a unique exploration of this Pacific paradise.`,
+    location: 'hawaii',
+    duration: 11,
+    cost: 1400,
+  },
+  {
+    id: 3,
+    image: tour3,
+    date: 'september 15th, 2020',
+    title: 'explore hong kong',
+    info: `Discover the dynamic blend of tradition and modernity in Hong Kong with our immersive Hong Kong Adventure. Explore the bustling streets, iconic skyscrapers, and cultural gems that define this vibrant metropolis. From the historic neighborhoods to the dazzling skyline, our adventure offers a glimpse into the unique fusion of East and West. Experience the energy of street markets, savor local cuisine, and witness the captivating contrast of tradition and innovation in the heart of Hong Kong.`,
+    location: 'hong kong',
+    duration: 8,
+    cost: 5000,
+  },
+  {
+    id: 4,
+    image: tour4,
+    date: 'december 5th, 2019',
+    title: 'kenya highlights',
+    info: `Embark on an extraordinary journey through the diverse landscapes of Kenya with our Kenya Safari Adventure. Witness the breathtaking beauty of savannahs, encounter iconic wildlife in their natural habitats, and experience the rich tapestry of cultures that define this East African gem. From the vast plains of the Maasai Mara to the majestic Mount Kenya, our safari adventure promises an immersive exploration of nature's wonders and the vibrant spirit of Kenya's people. Join us for an unforgettable safari experience in the heart of Africa.`,
+    location: 'kenya',
+    duration: 20,
+    cost: 3300,
+  },
+];
