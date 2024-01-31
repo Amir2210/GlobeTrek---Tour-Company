c32888ac15534531b2ec214a8621c51b28c800c3
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-01-31 12:50:48 +0200

    Make the nav-links in  the header to be dynamic based on the currnet path

--- src/cmps/MainHeader.jsx
+++ src/cmps/MainHeader.jsx
@@ -20,12 +20,16 @@ export function MainHeader() {
           </Link>
         )}
         <div className='nav-links'>
-          <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#services">Services</a></li>
-            <li><a href="#tours">Tours</a></li>
-          </ul>
+          {location.pathname === '/' ? (
+            <ul>
+              <li><a href="#home">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#services">Services</a></li>
+              <li><a href="#tours">Tours</a></li>
+            </ul>
+          ) : (
+            null
+          )}
         </div>
         <div className='nav-icons'>
           <ul>
